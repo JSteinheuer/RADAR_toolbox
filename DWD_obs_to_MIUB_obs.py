d2f9d2b06018abd6b32fd1ec001b7ebacdb82cf0
JSteinheuer <Julian.Steinheuer@gmx.de>
2024-01-10 17:07:35 +0100

    include JGiles github

--- DWD_obs_to_MIUB_obs.py
+++ DWD_obs_to_MIUB_obs.py
@@ -11,3 +11,12 @@
 
 import HEADER_RADAR_toolbox as header
 
+import sys
+sys.path.insert(0, header.dir_projects + 'RADAR_toolbox/radar_processing_scripts')
+# maybe awkward because >import utils< would work now, but the following
+# additionally enables  pycharm to word-completing (i.e., functions) since
+# the folder containing /radar_processing_scripts/ was already in the (global)
+# sys.path variable (i.e. the project folder).
+import radar_processing_scripts.utils as utils
+
+
